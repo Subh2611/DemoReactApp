24ec66f60656747f742ece29b904e40035196c41
subh2611 <subh2611@gmail.com>
2021-04-09 13:33:06 +0530

    Cognito User

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,9 +4,6 @@ export default function Footer() {
   return (
     <footer className="footer">
       <div className="content has-text-centered">
-        <p>
-           Hexal 2019. The source code is licensed MIT. The website content is licensed CC BY NC SA 4.0.
-        </p>
       </div>
     </footer>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,13 +1,24 @@
 import React, { Component } from 'react'
+import { Auth } from 'aws-amplify';
 
 export default class Navbar extends Component {
+
+  handleLogOut = async event => {
+    event.preventDefault();
+    try{
+      const user = await Auth.signOut();
+      this.props.auth.setAuthStatus(false);
+      this.props.auth.setUser(null);
+    }catch(error){
+      console.log(error);
+    }
+  }
+
   render() {
     return (
       <nav className="navbar" role="navigation" aria-label="main navigation">
         <div className="navbar-brand">
-          <a className="navbar-item" href="/">
-            <img src="hexal-logo.png" width="112" height="28" alt="hexal logo" />
-          </a>
+          
         </div>
 
         <div id="navbarBasicExample" className="navbar-menu">
@@ -18,20 +29,39 @@ export default class Navbar extends Component {
             <a href="/products" className="navbar-item">
               Products
             </a>
-            <a href="/admin" className="navbar-item">
+            {/* <a href="/admin" className="navbar-item">
               Admin
-            </a>
+            </a> */}
           </div>
 
           <div className="navbar-end">
             <div className="navbar-item">
+              {this.props.auth.isAuthenticated && this.props.auth.user && (
+                <p>
+                  Hello {this.props.auth.user.username}
+                </p>
+              )}
               <div className="buttons">
+              {!this.props.auth.isAuthenticated  && (
+                <div>
                 <a href="/register" className="button is-primary">
                   <strong>Register</strong>
                 </a>
                 <a href="/login" className="button is-light">
-                  Log in
+                  Log In
                 </a>
+                </div>
+                 )}
+                 {this.props.auth.isAuthenticated  && (
+                   <div>
+                  <a href="/" onClick={this.handleLogOut} className="button is-light">
+                  Log Out
+                </a>
+                 <a href="/changepassword" className="button is-light">
+                 Change Password
+               </a>
+               </div>
+                )}
               </div>
             </div>
           </div>
